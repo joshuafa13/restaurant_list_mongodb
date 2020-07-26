abb5e3eba8b1ee803bf89861cac052a66fbadd41
Josh Fang <joshuafang13@gmail.com>
2020-07-26 21:49:56 +0800

    feat: add seeder

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const restaurantSchema = new Schema({
+	id: {
+		type: Number,
+		required: true,
+	},
 	name: {
-		type: String, // 資料型別是字串
-		required: true, // 這是個必填欄位
+		type: String,
+		required: true,
 	},
 	name_en: {
 		type: String,
