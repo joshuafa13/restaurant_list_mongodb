8604ac98d37ccc1fe0671b137dacd12ee886fae9
Josh Fang <joshuafang13@gmail.com>
2020-07-26 21:38:24 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,15 +2,29 @@
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
+const mongoose = require('mongoose')
 
 // variables
 const port = 3000
 
-// set up express handlebars
+// setup mongoose
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
+// setup mongoose connection
+const db = mongoose.connection
+// connection error
+db.on('error', () => {
+	console.log('mongodb error')
+})
+// connection success
+db.once('open', () => {
+	console.log('mongodb connected')
+})
+
+// setup express handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
 app.set('view engine', 'hbs')
 
-// set up router
+// setup router
 app.get('/', (req, res) => {
 	res.render('index')
 })
