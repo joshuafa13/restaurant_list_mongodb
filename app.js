570810e0078c4afe12ab73979bc5d584bb15e8d0
Josh Fang <joshuafang13@gmail.com>
2020-07-31 00:31:34 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
 // include modules
 const express = require('express')
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose')
-const Restaurant = require('./models/restaurant.js')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const app = express()
 const routes = require('./routes')
-
-// setup mongoose
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-// setup mongoose connection
-const db = mongoose.connection
-// connection error
-db.on('error', () => {
-	console.log('mongodb error')
-})
-// connection success
-db.once('open', () => {
-	console.log('mongodb connected')
-})
+require('./config/mongoose')
 
 // setup express handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }))
@@ -30,7 +16,6 @@ app.use(express.static('public'))
 app.use(methodOverride('_method'))
 // body parser
 app.use(bodyParser.urlencoded({ extended: true }))
-
 // setup router
 app.use(routes)
 
---
+++ config/mongoose.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+// setup mongoose
+mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
+// setup mongoose connection
+const db = mongoose.connection
+// connection error
+db.on('error', () => {
+	console.log('mongodb error')
+})
+// connection success
+db.once('open', () => {
+	console.log('mongodb connected')
+})
+
+module.exports = db
